27445375872499db01d152fc9ba1715e80bbce1c
Abel Tony <122355309+AbelTony-dev@users.noreply.github.com>
2024-02-14 12:49:32 +0200

    fix

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ const fetchTwitterHandles = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error("Error fetching Twitter handles:", error);
