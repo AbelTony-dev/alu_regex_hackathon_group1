2bfc9434e1038e4748c629eb715daf03dcb188cc
Abel Tony <122355309+AbelTony-dev@users.noreply.github.com>
2024-02-14 12:48:36 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"; // Import the main App component
-
+import App from "./App"; 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> {/* Render the App component */}
+    <App /> 
   </React.StrictMode>,
-  document.getElementById("root") // Mount the App component to the root DOM element
+  document.getElementById("root") 
 );
